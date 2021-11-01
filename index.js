d49a371f69ce7def56b864a49b1252afd48ac989
ilwel <ilwelisaac@hotmail.com>
2021-11-01 11:42:05 -0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const router = require('./router');
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
